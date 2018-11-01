d04067fe5b3ceed2356a1775a103014432fcb11b
shannon <lucassha@oregonstate.edu>
2018-11-01 12:27:40 +1000

    updated env variables in docker-compose. either method wrks. will review in tutorials

--- web/app.js
+++ web/app.js
@@ -12,11 +12,11 @@ app.set('view engine', 'handlebars');
 /* set up sql connection */
 var mysql = require("mysql");
 var connection = mysql.createConnection({
-    host            : 'db',
-    port            : '3306',
-    user            : 'mysql',
-    password        : 'supersecret',
-    database        : 'stormlight'
+    host            : process.env.DATABASE_HOST,
+    port            : process.env.MYSQL_PORT,
+    user            : process.env.MYSQL_USER,
+    password        : process.env.MYSQL_PASSWORD,
+    database        : process.env.MYSQL_DATABASE
 });
 
 /* throw an error if sql connect fails. it should fail a couple times in docker 
